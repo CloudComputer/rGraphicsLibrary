f8110555097ec98b848116075bfce7638bf9bc91
Rickard Englund <gefle.rickard@gmail.com>
2013-08-04 18:27:16 +0200

    Added logger to rEngineScene

--- Examples/rEngineScene/rEngineScene.cpp
+++ Examples/rEngineScene/rEngineScene.cpp
@@ -10,6 +10,7 @@
 #include <Util\Directory.h>
 
 #include <Util\Macros.h>
+#include <Util\Logger.h>
 
 tinyxml2::XMLDocument doc;
 
@@ -19,6 +20,43 @@ void loadXML(rEngine *t,std::string filename){
 	auto firstChild = doc.FirstChildElement("rEngine");
 	if(firstChild){
 		bool windowCreated = false;
+        FOR_XML_ELE("loging",firstChild,l){
+            auto manager = LogManager::getLogManager();
+            auto eConsole = l->FirstChildElement("console");
+            auto eFile = l->FirstChildElement("file");
+            if(eConsole){
+                auto logger = new ConsolLogger();
+                auto lvl = eConsole->Attribute("logLevel");
+                auto timmer = eConsole->Attribute("useTimmers");
+                if(lvl && strcmp(lvl,"debug") == 0) logger->setMinLogLevel(Debug);
+                else if(lvl && strcmp(lvl,"info") == 0) logger->setMinLogLevel(Info);
+                else if(lvl && strcmp(lvl,"warning") == 0) logger->setMinLogLevel(Warning);
+                else if(lvl && strcmp(lvl,"error") == 0) logger->setMinLogLevel(Error);
+
+                logger->setUseTimmers(timmer && strcmp(timmer,"on") == 0);
+                manager->attatchLogger(logger);
+            }
+            if(eFile){
+                auto filename = eFile->Attribute("file");
+                if(filename){
+                    auto logger = FileLogger::CreateLogger(filename);
+                    if(logger){
+                        auto lvl = eFile->Attribute("logLevel");
+                        auto timmer = eFile->Attribute("useTimmers");
+                        if(lvl && strcmp(lvl,"debug") == 0) logger->setMinLogLevel(Debug);
+                        else if(lvl && strcmp(lvl,"info") == 0) logger->setMinLogLevel(Info);
+                        else if(lvl && strcmp(lvl,"warning") == 0) logger->setMinLogLevel(Warning);
+                        else if(lvl && strcmp(lvl,"error") == 0) logger->setMinLogLevel(Error);
+
+                        logger->setUseTimmers(timmer && strcmp(timmer,"on") == 0);
+                        manager->attatchLogger(logger);
+                    }
+                }
+            }
+
+            LOG_INFO("Logger initiated");
+            //break;
+        }
 		FOR_XML_ELE("window",firstChild,win){
 			rWindow::ReadXML(t,win);
 			windowCreated = true;
